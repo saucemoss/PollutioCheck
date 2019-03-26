4673723be4aad073018d99a63b56b0f82873836e
Maciej Sosnowski <Maciej.Sosnowski@lppsa.com>
2019-03-26 10:24:03 +0100

    Commit

---
+++ app/src/main/java/com/example/pollutiocheck/fetchData.java
@@ -0,0 +1,42 @@
+package com.example.pollutiocheck;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class fetchData extends AsyncTask<Void,Void,Void> {
+    String data = "";
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            URL url = new URL("http://api.gios.gov.pl/pjp-api/rest/station/findAll");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while(line != null){
+                line = bufferedReader.readLine();
+                data = data + line;
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        MainActivity.data.setText(data);
+    }
+}
