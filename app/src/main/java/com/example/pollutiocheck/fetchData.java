4144d48150f19003c76d1dfc74c48b7df529474f
Maciej Sosnowski <Maciej.Sosnowski@lppsa.com>
2019-03-27 15:46:27 +0100

    adding finished basic feature

--- app/src/main/java/com/example/pollutiocheck/fetchData.java
+++ app/src/main/java/com/example/pollutiocheck/fetchData.java
@@ -17,12 +17,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class fetchData extends AsyncTask<Void,Void,Void> {
-    String data = "";
+    private String data = "";
+    private String stationData = "";
     String dataParsed = "";
-    ArrayList<Integer> ids = new ArrayList<Integer>();
-    ArrayList<Double> zz = new ArrayList<Double>();
-
-
+    private ArrayList<Integer> ids = new ArrayList<Integer>();
+    private ArrayList<Double> offsetScore = new ArrayList<Double>();
 
     @Override
     protected Void doInBackground(Void... voids) {
@@ -36,39 +35,26 @@ public class fetchData extends AsyncTask<Void,Void,Void> {
                 line = bufferedReader.readLine();
                 data = data + line;
             }
-            Double[] lat = new Double[2000];
-            Double[] lon = new Double[2000];
+
+            Double[] lat = new Double[500];
+            Double[] lon = new Double[500];
             JSONArray JA = new JSONArray(data);
+
             for(int i = 0; i < JA.length(); i++){
                 Integer id = JA.getJSONObject(i).getInt("id");
                 String stationName = JA.getJSONObject(i).getString("stationName");
                 Double gegrLat = JA.getJSONObject(i).getDouble("gegrLat");
                 Double gegrLon = JA.getJSONObject(i).getDouble("gegrLon");
-
                 ids.add(i, id);
                 lat[i]=gegrLat;
                 lon[i]=gegrLon;
+                double offsetPointsLat = MainActivity.latitude - lat[i];
+                double offsetPointsLong = MainActivity.longitude - lon[i];
+                if(offsetPointsLong < 0) offsetPointsLong = offsetPointsLong * -1;
+                if(offsetPointsLat < 0) offsetPointsLat = offsetPointsLat * -1;
+                offsetScore.add(i, (offsetPointsLong + offsetPointsLat));
 
-                double x1 = MainActivity.latitude;
-                double y1 = MainActivity.longitude;
-                double x3 = 0;
-                double y3 = 0;
-                double x4 = 0;
-                double y4 = 0;
-
-                x3 = (x1 + lat[i])/2;
-                y3 = (y1 + lon[i])/2;
-
-                x4=x1-x3;
-                y4=y1-y3;
-
-                if(x4 < 0) x4 = x4 * -1;
-                if(y4 < 0) y4 = y4 * -1;
-                double z = x4 + y4;
-
-                zz.add(i, z);
-
-
+                /*  output info about all stations
                 dataParsed = dataParsed +
                                         "Station No: " +
                                         id + "\n" +
@@ -76,24 +62,21 @@ public class fetchData extends AsyncTask<Void,Void,Void> {
                                         stationName + "\n" +
                                         "Latitude, Longitude: " +
                                         gegrLat + ", " + gegrLon + "\n" + "\n";
-
+                */
             }
 
-            int minIndex = zz.indexOf(Collections.min(zz));
-            dataParsed = dataParsed + "\n" + "Nearest station, id: " + ids.get(minIndex);
+            int minIndex = offsetScore.indexOf(Collections.min(offsetScore));
+            dataParsed = dataParsed + "\n" + "Nearest station id: " + ids.get(minIndex);
 
             for(int i = 0; i < JA.length(); i++) {
                 Integer id = JA.getJSONObject(i).getInt("id");
                 String stationName = JA.getJSONObject(i).getString("stationName");
                 if(ids.get(minIndex) == JA.getJSONObject(i).getInt("id")){
-                    dataParsed = dataParsed +  ", Name: " + stationName;
+                    dataParsed +=  ", Name: " + stationName + "\n";
                 }
-
-
             }
 
-
-
+            getPollutionInfo(ids.get(minIndex));
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -104,6 +87,46 @@ public class fetchData extends AsyncTask<Void,Void,Void> {
         }
         return null;
     }
+
+    void getPollutionInfo(int index){
+
+        try {
+            URL urlStation = new URL("http://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/" + index);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) urlStation.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while(line != null) {
+                line = bufferedReader.readLine();
+                stationData += line;
+            }
+
+
+            JSONObject obj = new JSONObject(stationData);
+            String stCalcDate = obj.getString("stCalcDate");
+            String stIndexLevel = obj.getJSONObject("stIndexLevel").getString("indexLevelName");
+            String so2IndexLevel = obj.getJSONObject("so2IndexLevel").getString("indexLevelName");
+            String no2IndexLevel = obj.getJSONObject("no2IndexLevel").getString("indexLevelName");
+            String pm10IndexLevel = obj.getJSONObject("pm10IndexLevel").getString("indexLevelName");
+            String pm25IndexLevel = obj.getString("pm25IndexLevel");
+            dataParsed += "Ostatnia data pomiaru: " + stCalcDate +
+                            " \n Air pollution index: " + stIndexLevel +
+                            " \n Sulphur dioxide index: " + so2IndexLevel +
+                            " \n Nitrogen dioxide index: " + no2IndexLevel +
+                            " \n PM10 index: " + pm10IndexLevel +
+                            " \n PM25 index: " + pm25IndexLevel;
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            dataParsed += "JSON excepiton";
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void onPostExecute(Void aVoid) {
         super.onPostExecute(aVoid);
