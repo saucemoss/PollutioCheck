79c91dc750b050f24d7530494e7740650ec7a68f
Maciej Sosnowski <sosnowski.maciej@o2.pl>
2019-03-30 22:37:05 +0100

    bug fixes and code refactor

--- app/src/main/java/com/example/pollutiocheck/fetchData.java
+++ app/src/main/java/com/example/pollutiocheck/fetchData.java
@@ -18,16 +18,20 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class fetchData extends AsyncTask<Void,Void,Void> {
-    private String data = "";
-    private String stationData = "";
+    String data = "";
     String dataParsed = "";
-    private ArrayList<Integer> ids = new ArrayList<Integer>();
-    private ArrayList<Double> offsetScore = new ArrayList<Double>();
+    ArrayList<Integer> ids = new ArrayList<Integer>();
+    ArrayList<Double> offsetScore = new ArrayList<Double>();
+    Integer id = 0;
 
     @Override
     protected Void doInBackground(Void... voids) {
         try {
+
+            //set url for all statnios
             URL url = new URL("http://api.gios.gov.pl/pjp-api/rest/station/findAll");
+
+            //set up url connection and get all input to string
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
             InputStream inputStream = httpURLConnection.getInputStream();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
@@ -37,60 +41,24 @@ public class fetchData extends AsyncTask<Void,Void,Void> {
                 data = data + line;
             }
 
-            Double[] lat = new Double[500];
-            Double[] lon = new Double[500];
+            //set up variable for json array based on input from http
             JSONArray JA = new JSONArray(data);
 
-            for(int i = 0; i < JA.length(); i++){
-                Integer id = JA.getJSONObject(i).getInt("id");
-                String stationName = JA.getJSONObject(i).getString("stationName");
-                Double gegrLat = JA.getJSONObject(i).getDouble("gegrLat");
-                Double gegrLon = JA.getJSONObject(i).getDouble("gegrLon");
-                ids.add(i, id);
-                lat[i]=gegrLat;
-                lon[i]=gegrLon;
-                double offsetPointsLat = MainActivity.latitude - lat[i];
-                double offsetPointsLong = MainActivity.longitude - lon[i];
-                if(offsetPointsLong < 0) offsetPointsLong = offsetPointsLong * -1;
-                if(offsetPointsLat < 0) offsetPointsLat = offsetPointsLat * -1;
-                offsetScore.add(i, (offsetPointsLong + offsetPointsLat));
+            // get the lowest offsets score location in offset score array and store it as minIndex.
+            //int minIndex = offsetScore.indexOf(Collections.min(offsetScore));
 
-                /*  output info about all stations
-                dataParsed = dataParsed +
-                                        "Station No: " +
-                                        id + "\n" +
-                                        "Station name: " +
-                                        stationName + "\n" +
-                                        "Latitude, Longitude: " +
-                                        gegrLat + ", " + gegrLon + "\n" + "\n";
-                */
-            }
+            // get and printout ID from "ids" array based on location of lowest score in offset score array.
+            // dataParsed = dataParsed + "Nearest station id: " + ids.get(minIndex);
 
-            int minIndex = offsetScore.indexOf(Collections.min(offsetScore));
-            dataParsed = dataParsed + "Nearest station id: " + ids.get(minIndex);
-            for(int i = 0; i < JA.length(); i++) {
-                Integer id = JA.getJSONObject(i).getInt("id");
-                String stationName = JA.getJSONObject(i).getString("stationName");
-                if(ids.get(minIndex) == JA.getJSONObject(i).getInt("id")){
-                    dataParsed +=  ", Name: " + stationName;
-                }
-            }
+            // printout the nearest station name
+            // dataParsed += checkForName(JA, ids.get(minIndex));
 
-            getPollutionInfo(ids.get(minIndex));
+            // printout pollution of nearest station
+            //getPollutionInfo(ids.get(minIndex));
 
-            ArrayList<Double>offsetScoreSorted = new ArrayList<>(offsetScore);
-            Collections.sort(offsetScoreSorted);
-
-            for(int i = 1; i < 20; i++) {
-                Double ni = offsetScoreSorted.get(i);
-                for(int j = 0; j < offsetScoreSorted.size(); j++) {
-                    if (ni == offsetScore.get(j)){
-                        int id = offsetScore.indexOf(ni);
-                        dataParsed += "\n\nNext closest station: " + checkForName(JA, id);
-                    }
-                }
-                getPollutionInfo(ids.indexOf(offsetScore) +i + ids.indexOf(offsetScore));
-            }
+            // get 10 station ids nearest to user
+            //
+            getPollutionInfoList(JA, 10);
 
 
         } catch (MalformedURLException e) {
@@ -104,20 +72,72 @@ public class fetchData extends AsyncTask<Void,Void,Void> {
         return null;
     }
 
-    void getPollutionInfo(int index){
+    private void getStationsOffsetScore(JSONArray JA) throws JSONException {
+        //set up arrays for each coordinates
+        Double[] lat = new Double[500];
+        Double[] lon = new Double[500];
+
+        //iterate json array for coordinates data and ID of each polution reading station
+        for(int i = 0; i < JA.length(); i++){
+            // get the id and add it to array list "ids"
+            id = JA.getJSONObject(i).getInt("id");
+            ids.add(i, id);
+
+            //get coordinates and add them to arrays
+            Double gegrLat = JA.getJSONObject(i).getDouble("gegrLat");
+            Double gegrLon = JA.getJSONObject(i).getDouble("gegrLon");
+            lat[i]=gegrLat;
+            lon[i]=gegrLon;
+
+            // subtract coordinates of each station from user current location cooridnates and store them as "offsetPoints"
+            double offsetPointsLat = MainActivity.latitude - lat[i];
+            double offsetPointsLong = MainActivity.longitude - lon[i];
+
+            //  revers sign (-) if offset points are lower than 0
+            if(offsetPointsLong < 0) offsetPointsLong = offsetPointsLong * -1;
+            if(offsetPointsLat < 0) offsetPointsLat = offsetPointsLat * -1;
+
+            // add offset points of latitude and longitude to make up an final score for each station.
+            // The lower the offset score is, the closer a station is to user location
+            offsetScore.add(i, (offsetPointsLong + offsetPointsLat));
+        }
+    }
+
+    private void getPollutionInfoList(JSONArray JA, int iterations) throws JSONException {
+        getStationsOffsetScore(JA);
+        int i = 0;
+        while(i != iterations) {
+            // in list of ids, get index position of minimum value from offset score list
+            id = ids.get(offsetScore.indexOf(Collections.min(offsetScore)));
+            dataParsed += "Next closest station id " + id + ", " + checkForName(JA, id);
+
+            getPollutionInfo(id);
+
+            //remove previous id from ids list and minimum value from offset score list for next iteration
+            ids.remove(id);
+            offsetScore.remove(offsetScore.indexOf(Collections.min(offsetScore)));
+
+            dataParsed += "\n\n";
+            i++;
+        }
+    }
+
+    void getPollutionInfo(int id){
 
         try {
-            URL urlStation = new URL("http://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/" + index);
+            URL urlStation = new URL("http://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/" + id);
             HttpURLConnection httpURLConnection = (HttpURLConnection) urlStation.openConnection();
             InputStream inputStream = httpURLConnection.getInputStream();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
             String line = "";
-            while(line != null) {
+            String stationData = "";
+                while(line != null) {
                 line = bufferedReader.readLine();
                 stationData += line;
-            }
+                }
 
             JSONObject obj = new JSONObject(stationData);
+
             String stCalcDate = obj.getString("stCalcDate");
             dataParsed += "\nData collection date: " + stCalcDate;
             String[] indexList = {"stIndexLevel","so2IndexLevel","no2IndexLevel","pm10IndexLevel","pm25IndexLevel", "o3IndexLevel", "c6h6IndexLevel" };
@@ -126,7 +146,6 @@ public class fetchData extends AsyncTask<Void,Void,Void> {
 
                 dataParsed += paramsList[i] + checkForNull(obj, indexList[i]);
             }
-
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -145,19 +164,23 @@ public class fetchData extends AsyncTask<Void,Void,Void> {
             name = "No data available";
             return name;
         }
-
     }
 
-    String checkForName(JSONArray arr, int i) throws JSONException {
-        dataParsed += arr.getJSONObject(i).getString("stationName");
-        if(!arr.getJSONObject(i).getString("stationName").equals("null")) {
-            String stationName = arr.getJSONObject(i).getString("stationName");
-            return stationName;
-        } else {
-            String stationName = "No data available";
-            return stationName;
-        }
+    String checkForName(JSONArray arr, int id) throws JSONException {
+        String stationName = "";
+        for (int i = 0; i < arr.length(); i++) {
+            if (arr.getJSONObject(i).getInt("id") == id) {
 
+                if (!arr.getJSONObject(i).getString("stationName").equals("null")) {
+                    stationName = arr.getJSONObject(i).getString("stationName");
+
+                } else {
+                    stationName = "No data available";
+
+                }
+            }
+        }
+    return stationName;
     }
 
     @Override
